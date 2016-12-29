25df3932f36ea31dd677ef4eecee721cc1d9f2a2
Aliaksei Kasiyanik <aleksey.kasianik@gmail.com>
2016-12-29 19:47:20 +0300

    last fix

--- main.c
+++ main.c
@@ -1,19 +1,18 @@
 #include<mpi.h>
 #include<stdio.h>
 #include<stdlib.h>
-#include <time.h>
+#include<time.h>
 
 #define ROOT 0
 
 // procNum = 4
 // n = 20
 
-int Size = 4;
 double *A;
 double *B;
 double *C;
 
-void PrintMatrix(double *matr) {
+void PrintMatrix(double *matr, int Size) {
     for (int i = 0; i < Size; ++i) {
         for (int j = 0; j < Size; ++j) {
             printf("%7.4f ", matr[i * Size + j]);
@@ -37,15 +36,15 @@ void SeqMupliplication(double *A, double *B, int Size) {
     printf("\n");
 }
 
-void RandInit(double *matr, int n) {
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            matr[i * n + j] = (rand() % 100000) / 10000.0;
+void RandInit(double *matr, int Size) {
+    for (int i = 0; i < Size; ++i) {
+        for (int j = 0; j < Size; ++j) {
+            matr[i * Size + j] = (rand() % 100000) / 10000.0;
         }
     }
 }
 
-void InitMatrices(double *A, double *B, double *C) {
+void InitMatrices(double *A, double *B, double *C, int Size) {
     srand((unsigned) time(NULL));
     RandInit(A, Size);
     RandInit(B, Size);
@@ -54,18 +53,7 @@ void InitMatrices(double *A, double *B, double *C) {
     }
 }
 
-void Transpose(double *B) {
-    double temp;
-    for (int i = 0; i < Size; i++) {
-        for (int j = i + 1; j < Size; j++) {
-            temp = B[i * Size + j];
-            B[i * Size + j] = B[j * Size + i];
-            B[j * Size + i] = temp;
-        }
-    }
-}
-
-void LocalMultiplication(double *a, double *b, double *c, int ProcNum, int ProcRank, int IterNum, int RowsInTape) {
+void LocalMultiplication(double *a, double *b, double *c, int ProcNum, int ProcRank, int IterNum, int RowsInTape, int Size) {
     int BlockNumInC = (ProcRank + IterNum) % ProcNum;
     int Offset = BlockNumInC * RowsInTape;
     for (int i = 0; i < RowsInTape; ++i) {
@@ -81,6 +69,8 @@ void LocalMultiplication(double *a, double *b, double *c, int ProcNum, int ProcR
 
 void main(int argc, char *argv[]) {
 
+    int Size = atoi(argv[1]);
+
     int ProcNum, ProcRank;
     MPI_Init(&argc, &argv);
 
@@ -91,29 +81,35 @@ void main(int argc, char *argv[]) {
 
     int RowsInTape = Size / ProcNum;
     int ElementsInTape = Size * RowsInTape;
-//
+
     double *bufA = (double *) malloc(Size * RowsInTape * sizeof(double));
     double *bufB = (double *) malloc(Size * RowsInTape * sizeof(double));
     double *bufC = (double *) malloc(Size * RowsInTape * sizeof(double));
 
+    MPI_Datatype COLUMN_TYPE;
+    MPI_Datatype RESULT_COLUMN_TYPE;
     if (ProcRank == ROOT) {
         A = (double *) malloc(Size * Size * sizeof(double));
         B = (double *) malloc(Size * Size * sizeof(double));
         C = (double *) malloc(Size * Size * sizeof(double));
 
-        InitMatrices(A, B, C);
+        InitMatrices(A, B, C, Size);
         printf("Matrix A\n");
-        PrintMatrix(A);
+        PrintMatrix(A, Size);
         printf("Matrix B\n");
-        PrintMatrix(B);
-        Transpose(B);
+        PrintMatrix(B, Size);
+
+        MPI_Type_vector(Size, 1, Size, MPI_DOUBLE, &COLUMN_TYPE);
+        MPI_Type_commit(&COLUMN_TYPE);
+        MPI_Type_create_resized(COLUMN_TYPE, 0, 1 * sizeof(double), &RESULT_COLUMN_TYPE);
+        MPI_Type_commit(&RESULT_COLUMN_TYPE);
     }
 
     MPI_Scatter(A, ElementsInTape, MPI_DOUBLE, bufA, ElementsInTape, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
-    MPI_Scatter(B, ElementsInTape, MPI_DOUBLE, bufB, ElementsInTape, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
+    MPI_Scatter(B, RowsInTape, RESULT_COLUMN_TYPE, bufB, ElementsInTape, MPI_DOUBLE, ROOT, MPI_COMM_WORLD);
 
     int CurrIterNum = 0;
-    LocalMultiplication(bufA, bufB, bufC, ProcNum, ProcRank, CurrIterNum, RowsInTape);
+    LocalMultiplication(bufA, bufB, bufC, ProcNum, ProcRank, CurrIterNum, RowsInTape, Size);
 
     int NextProcRank = ProcRank == MaxProcRank ? ROOT : ProcRank + 1;
     int PrevProcRank = ProcRank == ROOT ? MaxProcRank : ProcRank - 1;
@@ -124,7 +120,7 @@ void main(int argc, char *argv[]) {
     for (CurrIterNum = 1; CurrIterNum < TotalIterCount; CurrIterNum++) {
         MPI_Sendrecv_replace(bufB, ElementsInTape, MPI_DOUBLE, NextProcRank, 0, PrevProcRank, 0, MPI_COMM_WORLD,
                              &Status);
-        LocalMultiplication(bufA, bufB, bufC, ProcNum, ProcRank, CurrIterNum, RowsInTape);
+        LocalMultiplication(bufA, bufB, bufC, ProcNum, ProcRank, CurrIterNum, RowsInTape, Size);
     }
 
     MPI_Gather(bufC, ElementsInTape, MPI_DOUBLE, C, ElementsInTape, MPI_DOUBLE, 0, MPI_COMM_WORLD);
@@ -135,10 +131,9 @@ void main(int argc, char *argv[]) {
 
     if (ProcRank == ROOT) {
         printf("Matrix C\n");
-        PrintMatrix(C);
+        PrintMatrix(C, Size);
 
         printf("TEST\n");
-        Transpose(B);
         SeqMupliplication(A, B, Size);
         free(A);
         free(B);
